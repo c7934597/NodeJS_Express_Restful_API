f58f42222b020c6e93ca2348a899df5e0440df81
c7934597 <c7934597@gmail.com>
2018-10-03 00:22:49 +0800

    API Error Test

--- src/server/controllers/user.controller.js
+++ src/server/controllers/user.controller.js
@@ -3,16 +3,16 @@ import bcrypt from 'bcrypt'
 import userModule from '../modules/user.module'
 
 /* User  POST 登入(Login) */
-const userLogin = (req, res) => {
+const userLogin = (req, res, next) => {
   // 取得帳密
   const insertValues = req.body
   userModule.selectUserLogin(insertValues).then((result) => {
     res.send(result) // 成功回傳result結果
-  }).catch((err) => { return res.send(err) }) // 失敗回傳錯誤訊息
+  }).catch((error) => { next(error) }) // 失敗回傳錯誤訊息
 }
 
 /* User  POST 新增 */
-const userPost = (req, res) => {
+const userPost = (req, res, next) => {
   // 取得新增參數
   const insertValues = {
     user_name: req.body.user_name,
@@ -21,34 +21,34 @@ const userPost = (req, res) => {
   }
   userModule.createUser(insertValues).then((result) => {
     res.send(result) // 成功回傳result結果
-  }).catch((err) => { return res.send(err) }) // 失敗回傳錯誤訊息
+  }).catch((error) => { next(error) }) // 失敗回傳錯誤訊息
 }
 
 /*  User GET 取得  */
-const userGet = (req, res) => {
+const userGet = (req, res, next) => {
   userModule.selectUser().then((result) => {
     res.send(result) // 成功回傳result結果
-  }).catch((err) => { return res.send(err) }) // 失敗回傳錯誤訊息
+  }).catch((error) => { next(error) }) // 失敗回傳錯誤訊息
 }
 
 /* User PUT 修改 */
-const userPut = (req, res) => {
+const userPut = (req, res, next) => {
   // 取得修改id
   const userId = req.params.user_id
   // 取得修改參數
   const insertValues = req.body
   userModule.modifyUser(insertValues, userId).then((result) => {
     res.send(result) // 回傳修改成功訊息
-  }).catch((err) => { return res.send(err) }) // 失敗回傳錯誤訊息
+  }).catch((error) => { next(error) }) // 失敗回傳錯誤訊息
 }
 
 /* User  DELETE 刪除 */
-const userDelete = (req, res) => {
+const userDelete = (req, res, next) => {
   // 取得刪除id
   const userId = req.params.user_id
   userModule.deleteUser(userId).then((result) => {
     res.send(result) // 回傳刪除成功訊息
-  }).catch((err) => { return res.send(err) }) // 失敗回傳錯誤訊息
+  }).catch((error) => { next(error) }) // 失敗回傳錯誤訊息
 }
 
 export default {
--- src/server/modules/user.module.js
+++ src/server/modules/user.module.js
@@ -1,6 +1,7 @@
 // user.module.js
 import bcrypt from 'bcrypt'
 import mysql from 'mysql'
+import APPError from '../helper/AppError'
 import config from '../../config/config'
 
 const connectionPool = mysql.createPool({
@@ -25,7 +26,7 @@ const selectUserLogin = (insertValues) => {
               console.error('SQL error: ', error)
               reject(error) // 寫入資料庫有問題時回傳錯誤
             } else if (Object.keys(result).length === 0) {
-              resolve('信箱尚未註冊！')
+              reject(new APPError.LoginError1()) // 信箱尚未註冊
             } else {
               const dbHashPassword = result[0].user_password // 資料庫加密後的密碼
               const userPassword = insertValues.user_password // 使用者登入輸入的密碼
@@ -33,7 +34,7 @@ const selectUserLogin = (insertValues) => {
                 if (res) {
                   resolve('登入成功') // 登入成功
                 } else {
-                  resolve('您輸入的密碼有誤！') // 登入失敗
+                  reject(new APPError.LoginError2()) // 登入失敗 輸入的密碼有誤
                 }
               })
             }
